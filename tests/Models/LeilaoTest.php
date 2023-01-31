00ec0730178ee728655c23fa672bf466720bc34b
Murillo Aguiar <aguiar_murillo@outlook.com>
2023-01-31 10:40:29 -0300

    Adicionar testes para recebeLance

---
+++ tests/Models/LeilaoTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Alura\Leilao\Tests\Models;
+
+use Alura\Leilao\Model\Lance;
+use Alura\Leilao\Model\Leilao;
+use Alura\Leilao\Model\Usuario;
+use PHPUnit\Framework\TestCase;
+
+class LeilaoTest extends TestCase{
+
+    /**
+     * @dataProvider geraLances
+     */
+    public function testLeilaoDeveReceberLances(int $qtdLances, Leilao $leilao, array $valores)
+    {
+        
+        static::assertCount($qtdLances, $leilao->getLances());
+
+        foreach ($valores as $key => $value) {
+            static::assertEquals($value, $leilao->getLances()[$key]->getValor());
+        }
+        
+    }
+
+    public function geraLances()
+    {
+        $joao = new Usuario('João');
+        $maria = new Usuario('Maria');
+
+        $leilaoCom2Lances = new Leilao('Fiat 147 0km');
+        $leilaoCom2Lances->recebeLance(new Lance($joao, 1000));
+        $leilaoCom2Lances->recebeLance(new Lance($maria, 2000));
+
+        $leilaoCom1Lance = new Leilao('Fusca 1972 0km');
+        $leilaoCom1Lance->recebeLance(new Lance($maria, 5000));
+
+        return [
+            "2-lances"=>[2, $leilaoCom2Lances, [1000,2000]],
+            "1-lance"=>[1, $leilaoCom1Lance, [5000]],
+        ];
+    }
+}
