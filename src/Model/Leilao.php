c3772f84a836c4bdf0e413b4834e34bf582c2d96
Murillo Aguiar <aguiar_murillo@outlook.com>
2023-02-01 10:38:18 -0300

    Implementar:leilão n recebe 5 lances de um usuario

--- src/Model/Leilao.php
+++ src/Model/Leilao.php
@@ -4,7 +4,7 @@ namespace Alura\Leilao\Model;
 
 class Leilao
 {
-    /** @var Lance[] */
+    
     private $lances;
     /** @var string */
     private $descricao;
@@ -20,6 +20,21 @@ class Leilao
         if (!empty($this->lances) && $this->ehDoUltimoUsuario($lance)) {
             return;
         }
+
+        $usuario = $lance->getUsuario();
+        $totalLancesUsuario = array_reduce($this->lances,
+            function(int $totalAcumulado, Lance $lanceAtual) use ($usuario){
+                if ($lanceAtual->getUsuario() == $usuario) {
+                    return $totalAcumulado + 1;
+                }
+                return $totalAcumulado;
+            },0
+        );
+
+        if ($totalLancesUsuario >=5) {
+            return;
+        }
+
         $this->lances[] = $lance;
     }
 
